b25b4f9d0571bbee0acf0b6fb385a6baa757d203
saeed jeddi <saeedjeddimail@gmail.com>
2023-05-05 13:03:24 +0330

    3

--- home/models.py
+++ home/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 
@@ -8,3 +9,24 @@ class Person(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Question(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='questions')
+    title = models.CharField(max_length=250,)
+    slug = models.SlugField(max_length=250)
+    body = models.TextField()
+    Created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+class Answer(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='answers')
+    questions = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='answers')
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.questions.title
+
--- home/serializers.py
+++ home/serializers.py
@@ -1,7 +1,20 @@
 from rest_framework import serializers
+from .models import Question, Answer
 
 
 class PersonsSerialize(serializers.Serializer):
     name = serializers.CharField(max_length=35)
     age = serializers.IntegerField()
-    email = serializers.EmailField() 
+    email = serializers.EmailField()
+
+
+class QuestionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Question
+        fields = '__all__'
+
+
+class AnswerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Answer
+        fields = '__all__'
